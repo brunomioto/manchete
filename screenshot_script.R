24590c9e49e159d0020d47d4c2216ca4fe38dc62
Bruno Mioto <brunomioto97@gmail.com>
2022-07-17 18:35:04 -0300

    add poder360

--- screenshot_script.R
+++ screenshot_script.R
@@ -3,12 +3,11 @@ library(webshot2)
 # Lista portais
 portais <- data.frame(
   url = c("https://www.folha.uol.com.br",
-          "https://www.estadao.com.br"),
-  file = paste0("./screenshots/",c("folha","estadao"),".png")
+          "https://www.estadao.com.br",
+          "https://www.poder360.com.br"),
+  file = paste0("./screenshots/",c("folha","estadao", "poder360"),".png")
 )
-
 
 # Efetua screenshot
 webshot2::webshot(url = portais$url, vwidth = 1500, vheight = 2000,
                   file = portais$file, delay = 10, cliprect = "viewport")
-  
