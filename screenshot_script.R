fca4b06babd84d5bd7c8498cf5b02a31585be3b1
Bruno Mioto <brunomioto97@gmail.com>
2022-07-17 18:25:00 -0300

    delete loop and add vector again

--- screenshot_script.R
+++ screenshot_script.R
@@ -2,34 +2,13 @@ library(webshot2)
 
 # Lista portais
 portais <- data.frame(
-  url = "https://www.folha.uol.com.br",
-  file = "./screenshots/folha.png"
+  url = c("https://www.folha.uol.com.br",
+          "https://www.estadao.com.br/")
+  file = paste("./screenshots/",c("folha","estadao"),".png")
 )
 
-# Cria variaveis para guardar as condicoes do loop
-cond_files <- TRUE
-cond_tries <- TRUE
-tries <- 0
 
-# Tenta por varias vezes fazer o screeshot das paginas
-while((cond_files & cond_tries)) {
+# Efetua screenshot
+webshot2::webshot(url = portais$url, vwidth = 1500, vheight = 2000,
+                  file = portais$file, delay = 10, cliprect = "viewport")
   
-  # Atualiza indicador de tentativas
-  tries <- tries + 1
-  
-  # Efetua screenshot
-  webshot2::webshot(url = portais$url, vwidth = 1500, vheight = 2000,
-                    file = portais$file, delay = 10, cliprect = "viewport")
-  
-  # Confirma que todas imagens foram criadas
-  nomes <- list.files()
-  files <- sum(portais$file %in% nomes)
-  cond_files <- files != dim(portais)[1]
-  
-  # Atualiza condicao da tentativa
-  cond_tries <- tries != 10
-  
-  # Realiza uma pausa antes da proxima tentativa
-  Sys.sleep(5)
-  
-}
