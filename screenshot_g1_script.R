d94a4b712abb7202e38fc17b511659b74e3729dc
Bruno Mioto <brunomioto97@gmail.com>
2022-07-17 17:39:05 -0300

    Update screenshot_g1_script.R

--- screenshot_g1_script.R
+++ screenshot_g1_script.R
@@ -3,5 +3,5 @@ library(webshot2)
 
 # Efetua screenshot
 webshot2::webshot(url = "https://g1.globo.com/", vwidth = 1500, vheight = 2000,
-                  file = "./screenshots/g1.png", delay = 15, cliprect = "viewport")
+                  file = "./screenshots/g1.png", delay = 25, cliprect = "viewport")
   
