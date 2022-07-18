a0f29cf9963940ded7d290c665fc7c237c51ca29
Bruno Mioto <brunomioto97@gmail.com>
2022-07-18 01:25:08 -0300

    add date and time

--- telegram.R
+++ telegram.R
@@ -6,7 +6,11 @@ bot <- TGBot$new(token = bot_token('MANCHETE_BOT'))
 bot$set_default_chat_id(-1001563823686)
 
 #send message
-bot$sendMessage('Bom dia! Essas são as principais manchetes de hoje')
+bot$sendMessage(paste0('Bom dia! Essas são as principais manchetes do dia ',
+                       format(Sys.time(), format="%d/%m/%Y", tz = "America/Sao_Paulo"),
+                       ' às ',
+                       format(Sys.time(), format="%X", tz = "America/Sao_Paulo")
+                       ))
 
 bot$sendPhoto('folha.png')
 bot$sendPhoto('g1.png')
