4f6705ad569103bcdb8c0bce2551f13e09fd5558
Bruno Mioto <brunomioto97@gmail.com>
2022-07-18 15:00:10 -0300

    tier2

--- twitter.R
+++ twitter.R
@@ -20,19 +20,47 @@ tier_1_files <- grep("_tier1", filelist, value = TRUE)
 
 # Build the status message
 tier_1_tweet_text <- paste0("Manchetes do dia ", format(Sys.time(), format="%d/%m/%Y %X", tz = "America/Sao_Paulo"),"\n\n",
-                            ifelse(isTRUE(grep("folha", tier_1_files, value = TRUE) == "screenshots/foto1_folha_tier1.png") == TRUE,
+                            ifelse(isTRUE(grep("globo", tier_1_files, value = TRUE) == "screenshots/foto1_oglobo_tier1.png") == TRUE,
+                                   "- O Globo",""),
+                            ifelse(isTRUE(grep("folha", tier_1_files, value = TRUE) == "screenshots/foto2_folha_tier1.png") == TRUE,
                                    "- Folha de S. Paulo\n",""),
-                            ifelse(isTRUE(grep("estadao", tier_1_files, value = TRUE) == "screenshots/foto2_estadao_tier1.png") == TRUE,
+                            ifelse(isTRUE(grep("estadao", tier_1_files, value = TRUE) == "screenshots/foto3_estadao_tier1.png") == TRUE,
                                    "- O Estado de S. Paulo\n",""),
                             ifelse(isTRUE(grep("g1", tier_1_files, value = TRUE) == "screenshots/foto4_g1_tier1.png") == TRUE,
-                                   "- G1\n",""),
-                            ifelse(isTRUE(grep("poder360", tier_1_files, value = TRUE) == "screenshots/foto3_poder360_tier1.png") == TRUE,
-                                   "- Poder360","")
+                                   "- G1\n","")
 )
 
 # Post the status message to Twitter
 rtweet::post_tweet(
   status = tier_1_tweet_text,
   media = tier_1_files,
+  token = manchete_token
+)
+
+## TIER 2
+
+#get file names
+req <- httr::GET("https://api.github.com/repos/brunomioto/manchete/git/trees/master?recursive=1")
+httr::stop_for_status(req) 
+filelist <- unlist(lapply(httr::content(req)$tree, "[", "path"), use.names = F)
+
+tier_2_files <- grep("_tier2", filelist, value = TRUE)
+
+# Build the status message
+tier_2_tweet_text <- paste0("Manchetes do dia ", format(Sys.time(), format="%d/%m/%Y %X", tz = "America/Sao_Paulo"),"\n\n",
+                            ifelse(isTRUE(grep("istoe", tier_2_files, value = TRUE) == "screenshots/foto1_istoe_tier2.png") == TRUE,
+                                   "- IstoÉ\n",""),
+                            ifelse(isTRUE(grep("metropoles", tier_2_files, value = TRUE) == "screenshots/foto2_metropoles_tier2.png") == TRUE,
+                                   "- Metrópoloes\n",""),
+                            ifelse(isTRUE(grep("poder360", tier_2_files, value = TRUE) == "screenshots/foto3_poder360_tier2.png") == TRUE,
+                                   "- Poder360",""),
+                            ifelse(isTRUE(grep("extra", tier_2_files, value = TRUE) == "screenshots/foto4_extra_tier2.png") == TRUE,
+                                   "- Extra\n","")
+)
+
+# Post the status message to Twitter
+rtweet::post_tweet(
+  status = tier_2_tweet_text,
+  media = tier_2_files,
   token = manchete_token
 )
