c76d1c2b3cd07df4a3cc736f10b25d08daad9cff
Bruno Mioto <brunomioto97@gmail.com>
2022-07-18 00:56:59 -0300

    updating telegram script

--- telegram.R
+++ telegram.R
@@ -3,5 +3,7 @@ library(telegram)
 #connect manchete_bot
 bot <- TGBot$new(token = bot_token('manchete_bot'))
 
+bot$set_default_chat_id(-1001563823686)
+
 #send message
-bot$sendMessage('This is plain text')
+bot$sendMessage('oi')
