3e138fed0bc0550546d15cd41f1110478e8a44e1
Bruno Mioto <brunomioto97@gmail.com>
2022-07-17 22:34:54 -0300

    twitter

---
+++ twitter.R
@@ -0,0 +1,20 @@
+library(rtweet)
+
+manchete_token <- rtweet::create_token(
+  app = "Manchete",
+  consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+# Build the status message
+tweet_text <- paste0(
+  "test"
+)
+
+# Post the status message to Twitter
+rtweet::post_tweet(
+  status = tweet_text,
+  token = manchete_token
+)
