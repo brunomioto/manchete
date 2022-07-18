d06f2c9f1dccb730ca1871091dc2e19bbdd3d662
Icaro Bernardes <asaicaro@gmail.com>
2022-07-18 10:45:47 -0300

    screenshot feito com purrr

--- screenshot_script.R
+++ screenshot_script.R
@@ -1,13 +1,28 @@
 library(webshot2)
+library(purrr)
 
 # Lista portais
 portais <- data.frame(
   url = c("https://www.folha.uol.com.br",
           "https://www.estadao.com.br",
-          "https://www.poder360.com.br"),
-  file = paste0("./screenshots/",c("folha","estadao", "poder360"),".png")
+          "https://www.poder360.com.br",
+          "https://g1.globo.com/"),
+  file = paste0("./screenshots/",c("folha","estadao", "poder360", "g1"),".png"),
+  nome = c("Folha", "Estadão", "Poder360", "G1")
 )
 
-# Efetua screenshot
-webshot2::webshot(url = portais$url, vwidth = 1500, vheight = 2000,
-                  file = portais$file, delay = 10, cliprect = "viewport")
+# Cria um wrapper em torno da função de screenshot que inclui delay e torna a função "verbose"
+paparazzi <- function(url, file, nome) {
+  print(paste0("Fotografando ", nome, "..."))
+  
+  webshot2::webshot(url = url, vwidth = 1500, vheight = 2000,
+                    file = file, delay = 25, cliprect = "viewport")
+  
+  Sys.sleep(5)
+}
+
+# Efetua o screenshot, um portal por vez
+purrr::pwalk(
+  .l = portais,
+  .f = paparazzi
+)
