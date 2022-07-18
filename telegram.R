c402f835de22d39024013f7337a6d294c76713a3
Bruno Mioto <brunomioto97@gmail.com>
2022-07-18 01:21:03 -0300

    add photos

--- telegram.R
+++ telegram.R
@@ -6,4 +6,9 @@ bot <- TGBot$new(token = bot_token('MANCHETE_BOT'))
 bot$set_default_chat_id(-1001563823686)
 
 #send message
-bot$sendMessage('oi')
+bot$sendMessage('Bom dia! Essas são as principais manchetes de hoje')
+
+bot$sendPhoto('folha.png')
+bot$sendPhoto('g1.png')
+bot$sendPhoto('estadao.png')
+bot$sendPhoto('poder360.png')
