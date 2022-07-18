fbd6ae3bb6e87f718a55b9caaa72b752191141e1
Bruno Mioto <brunomioto97@gmail.com>
2022-07-18 13:05:17 -0300

    without text

--- twitter.R
+++ twitter.R
@@ -18,21 +18,21 @@ filelist <- unlist(lapply(httr::content(req)$tree, "[", "path"), use.names = F)
 
 tier_1_files <- grep("_tier1", filelist, value = TRUE)
 
-# Build the status message
-tier_1_tweet_text <- paste0("Manchetes do dia ", format(Sys.time(), format="%d/%m/%Y %X", tz = "America/Sao_Paulo"),"\n\n",
-                            ifelse(grep("folha", tier_1_files, value = TRUE) == "screenshots/foto1_folha_tier1.png",
-                                   "- Folha de S. Paulo\n",""),
-                            ifelse(grep("estadao", tier_1_files, value = TRUE) == "screenshots/foto2_estadao_tier1.png",
-                                   "- O Estado de S. Paulo\n",""),
-                            ifelse(grep("poder360", tier_1_files, value = TRUE) == "screenshots/foto3_poder360_tier1.png",
-                                   "- Poder360",""),
-                            ifelse(grep("g1", tier_1_files, value = TRUE) == "screenshots/foto4_g1_tier1.png",
-                                   "- G1\n","")
-)
+## Build the status message
+#tier_1_tweet_text <- paste0("Manchetes do dia ", format(Sys.time(), format="%d/%m/%Y %X", tz = "America/Sao_Paulo"),"\n\n",
+#                            ifelse(grep("folha", tier_1_files, value = TRUE) == "screenshots/foto1_folha_tier1.png",
+#                                   "- Folha de S. Paulo\n",""),
+#                            ifelse(grep("estadao", tier_1_files, value = TRUE) == "screenshots/foto2_estadao_tier1.png",
+#                                   "- O Estado de S. Paulo\n",""),
+#                            ifelse(grep("poder360", tier_1_files, value = TRUE) == "screenshots/foto3_poder360_tier1.png",
+#                                   "- Poder360",""),
+#                            ifelse(grep("g1", tier_1_files, value = TRUE) == "screenshots/foto4_g1_tier1.png",
+#                                   "- G1\n","")
+#)
 
 # Post the status message to Twitter
 rtweet::post_tweet(
-  status = tier_1_tweet_text,
+  #status = tier_1_tweet_text,
   media = tier_1_files,
   token = manchete_token
 )
