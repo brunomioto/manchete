1090cb4d6a3f34fd5a190c5ee787006725a49971
Bruno Mioto <brunomioto97@gmail.com>
2022-07-18 11:43:54 -0300

    Delete screenshot_g1_script.R

--- screenshot_g1_script.R
+++
@@ -1,7 +0,0 @@
-library(webshot2)
-
-
-# Efetua screenshot
-webshot2::webshot(url = "https://g1.globo.com/", vwidth = 1500, vheight = 2000,
-                  file = "./screenshots/g1.png", delay = 25, cliprect = "viewport")
-  
