fe1f15c4b1a470756232c04d0b3a9fbdd53b092f
Ícaro Bernardes <IcaroBernardes@users.noreply.github.com>
2022-07-18 22:35:19 -0300

    Update telegram.R

--- telegram.R
+++ telegram.R
@@ -12,7 +12,12 @@ bot$sendMessage(paste0('Olá! Essas são as principais manchetes do dia ',
                        format(Sys.time(), format="%X", tz = "America/Sao_Paulo")
                        ))
 
-bot$sendPhoto('./screenshots/folha.png')
-bot$sendPhoto('./screenshots/g1.png')
-bot$sendPhoto('./screenshots/estadao.png')
-bot$sendPhoto('./screenshots/poder360.png')
+bot$sendPhoto('./screenshots/foto1_oglobo_tier1.png')
+bot$sendPhoto('./screenshots/foto2_folha_tier1.png')
+bot$sendPhoto('./screenshots/foto3_estadao_tier1.png')
+bot$sendPhoto('./screenshots/foto4_g1_tier1.png')
+
+bot$sendPhoto('./screenshots/foto1_istoe_tier2.png')
+bot$sendPhoto('./screenshots/foto2_metropoles_tier2.png')
+bot$sendPhoto('./screenshots/foto3_poder360_tier2.png')
+bot$sendPhoto('./screenshots/foto4_extra_tier2.png')
